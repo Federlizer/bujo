34f5dbf4605932253fd67d2f7d702ae798638f9b
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-05-29 11:28:59 +0200

    Create sequelize task model

---
+++ src/db/Task.ts
@@ -0,0 +1,36 @@
+import sequelize from './index';
+import { Model, DataTypes } from 'sequelize';
+
+class Task extends Model {
+  public id!: number;
+  public text!: string;
+  public date!: Date;
+  public completed!: boolean;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAd!: Date;
+}
+
+Task.init({
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  text: {
+    type: new DataTypes.STRING(128),
+    allowNull: false,
+  },
+  date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  completed: {
+    type: DataTypes.BOOLEAN,
+  },
+}, {
+  sequelize,
+  tableName: 'tasks',
+});
+
+export default Task;
---
+++ src/db/index.ts
@@ -0,0 +1,5 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize('mariadb://root:XFXdoubled7770@localhost:3306/bujo');
+
+export default sequelize;
