e9cc9c19ca8c4e63d9855e97e99d92ff54b59481
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-06-04 13:50:24 +0200

    Configure express app to accept json

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,14 @@ import apiRouter from './routers/v1';
 
 const app = express();
 
+// general config
+app.use(express.json());
 app.use(function (req, _, next) {
   console.log(`[${req.method}] ${req.path}`);
   next();
 });
 
+// setup routes
 app.use('/v1', apiRouter);
 
 export default app;
