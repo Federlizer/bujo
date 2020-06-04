9eb952b523236d8905a39d677229a4b7bf03f143
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-06-04 14:27:28 +0200

    Initialize DB tables on startup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import db from './db';
 import app from './app';
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log("listening");
-});
+db.sync() // create tables if they don't exist already
+  .then(() => {
+    app.listen(port, () => {
+      console.log("listening");
+    });
+  })
+  .catch((err: Error) => {
+    console.error(err);
+  });
+
